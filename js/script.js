bb9ef8ba891980f55e5d6804997fa18f0e70d460
Ashley Cove <ash.cove99@gmail.com>
2022-05-06 08:00:19 +0100

    Added infoEl, infoEl.foreach, infoTextEl to js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,18 @@
 const accordion = document.querySelectorAll('.accordion-btn');
 const accQuestion = document.querySelectorAll('accordion-question');
 const arrowIcon = document.querySelectorAll('.icon-arrow');
+const infoEl = document.querySelectorAll('.info-text');
 
 accordion.forEach(btn =>
   btn.addEventListener('click', e => {
     const clicked = e.target.closest('.accordion-btn');
-    console.log(clicked);
 
-    clicked.classList.add('active');
+    if (!clicked) return;
+
+    infoEl.forEach(e => e.classList.remove('active'));
+
+    const infoTextEl = clicked.parentElement.nextElementSibling.children[0];
+
+    infoTextEl.classList.add('active');
   })
 );
